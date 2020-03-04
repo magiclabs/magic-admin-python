fd3c053af2eacd7ca00cd0a03cb7d50f43a88928
Arthur Jen <arthur@fortmatic.com>
2020-03-04 10:44:36 -0800

    Reserve package name

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='magic-sdk',
+    version='0.0.0.0',
+    description='Troll Goat',
+    url='https://troll_goat.TM',
+    author='Troll Goat',
+    author_email='arthur@fortmatic.com',
+    license='MIT',
+    packages=['magic-sdk'],
+    zip_safe=False,
+)
