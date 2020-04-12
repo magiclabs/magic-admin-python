1ab7fee95b9f0dc33ef11a93f34cc66357e4506b
Arthur Jen <arthur@fortmatic.com>
2020-04-11 20:02:55 -0700

    Rename to magic-admin

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='magic-sdk',
+    name='magic-admin',
     version='0.0.0.0',
     description='Troll Goat',
     url='https://troll_goat.TM',
     author='Troll Goat',
     author_email='arthur@fortmatic.com',
     license='MIT',
-    packages=['magic-sdk'],
+    packages=['magic_admin'],
     zip_safe=False,
 )
