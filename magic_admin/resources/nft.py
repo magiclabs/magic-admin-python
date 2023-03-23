3e5b3a3db1189c0991a3b8d19ab8284016bb0277
sandeepsuresh2020 <87987471+sandeepsuresh2020@users.noreply.github.com>
2023-03-23 14:37:57 -0400

    Sandeep ch73342 Mint Admin SDK (#66)
    * New resource file.
    
    * Unit tests for start mint.
    
    * Updating changelog and version.
    
    * flake8 fixes
    
    * More flake8 fixes.
    
    * Changing names, addressing comments.
    
    * changing to nft module.
    
    * Reorder
    
    * More detailed docstring
    
    * Asserting on resp

---
+++ magic_admin/resources/nft.py
@@ -0,0 +1,80 @@
+"""Module for the Mint resource component. Mint ERC721 and ERC1155 tokens."""
+from magic_admin.resources.base import ResourceComponent
+
+
+class NFT(ResourceComponent):
+    """Minting resource component."""
+
+    v1_start_mint721 = '/v1/admin/nft/mint/721_mint'
+    v1_start_mint1155 = '/v1/admin/nft/mint/1155_mint'
+
+    def start_mint721(
+        self,
+        contract_id: str,
+        quantity: int,
+        destination_address: str,
+    ):
+        """Mint ERC721 tokens.
+
+        Args:
+            contract_id (str): The contract ID of the ERC721 token.
+            quantity (int): The number of tokens to mint.
+            destination_address (str): The address to mint the tokens to.
+
+        Returns:
+            {
+                data: {
+                    request_id: str,
+                }, # noqa: RST203
+                status: str,
+                error_code: str,
+                message: str,
+            }
+
+        """
+        return self.request(
+            'post',
+            self.v1_start_mint721,
+            params={
+                'contract_id': contract_id,
+                'quantity': quantity,
+                'destination_address': destination_address,
+            },
+        )
+
+    def start_mint1155(
+        self,
+        contract_id: str,
+        quantity: int,
+        token_id: int,
+        destination_address: str,
+    ):
+        """Mint ERC1155 tokens.
+
+        Args:
+            contract_id (str): The contract ID of the ERC1155 token.
+            quantity (int): The number of tokens to mint.
+            token_id (int): The token ID of the ERC1155 token.
+            destination_address (str): The address to mint the tokens to.
+
+        Returns:
+            {
+                data: {
+                    request_id: str,
+                }, # noqa: RST203
+                status: str,
+                error_code: str,
+                message: str,
+            }
+
+        """
+        return self.request(
+            'post',
+            self.v1_start_mint1155,
+            params={
+                'contract_id': contract_id,
+                'quantity': quantity,
+                'token_id': token_id,
+                'destination_address': destination_address,
+            },
+        )
---
+++ tests/unit/resources/nft_test.py
@@ -0,0 +1,69 @@
+"""Test Mint resource component."""
+from unittest import mock
+
+import pytest
+
+from magic_admin.resources.nft import NFT
+
+
+class TestNFT:  # noqa: WPS306
+    """Test Mint resource component."""
+
+    quantity = 2
+    destination_address = '0x3c15B0e0e00A9edD2Be824064f9C9C29fc136C4E'
+    token_id = 1
+    nft = NFT()
+    contract_id = 'bsdjfkn-sjknfskn-kjsnf'
+    request_id = '7802kfn-kshbfdskjbf-sljdfksjb'
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.nft.request = mock.Mock(
+            return_value={
+                'data': {
+                    'request_id': self.request_id,
+                },
+            },
+        )
+
+    def test_start_mint721(self):
+        """Test mint 721."""
+        resp = self.nft.start_mint721(
+            self.contract_id,
+            self.quantity,
+            self.destination_address,
+        )
+
+        self.nft.request.assert_called_once_with(
+            'post',
+            self.nft.v1_start_mint721,
+            params={
+                'contract_id': self.contract_id,
+                'quantity': self.quantity,
+                'destination_address': self.destination_address,
+            },
+        )
+
+        assert resp.get('data').get('request_id') == self.request_id
+
+    def test_start_mint1155(self):
+        """Test mint 1155."""
+        resp = self.nft.start_mint1155(
+            self.contract_id,
+            self.quantity,
+            self.token_id,
+            self.destination_address,
+        )
+
+        self.nft.request.assert_called_once_with(
+            'post',
+            self.nft.v1_start_mint1155,
+            params={
+                'contract_id': self.contract_id,
+                'quantity': self.quantity,
+                'token_id': self.token_id,
+                'destination_address': self.destination_address,
+            },
+        )
+
+        assert resp.get('data').get('request_id') == self.request_id
