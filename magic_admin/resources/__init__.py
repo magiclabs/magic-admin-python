04f30e43df86312946385225f571f79a98f4cc24
sandeepsuresh2020 <87987471+sandeepsuresh2020@users.noreply.github.com>
2023-03-24 15:58:18 -0400

    Patch to NFT module (#68)

--- magic_admin/resources/__init__.py
+++ magic_admin/resources/__init__.py
@@ -1,3 +1,4 @@
+from magic_admin.resources.nft import NFT
 from magic_admin.resources.token import Token
 from magic_admin.resources.user import User
 from magic_admin.resources.wallet import WalletType
