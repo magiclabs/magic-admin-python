e092355ccdc9790599edbc379f886161415000c1
Ben Griffin <ben.griffin@magic.link>
2023-05-02 12:38:47 -0400

    [FIX]: removing unneeded NFT code

--- magic_admin/resources/__init__.py
+++ magic_admin/resources/__init__.py
@@ -1,4 +1,3 @@
-from magic_admin.resources.nft import NFT
 from magic_admin.resources.token import Token
 from magic_admin.resources.user import User
 from magic_admin.resources.wallet import WalletType
